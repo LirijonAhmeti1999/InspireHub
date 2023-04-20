ee15ed50a5bad86b3068f684659bea5f04b9fe04
Lirijon Ahmeti <la45454@ubt-uni.net>
2023-04-20 18:48:58 +0200

    header css logo js

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,21 @@
 function openNavLinks() {
   var element = document.getElementById("top-header");
+  var mainBody = document.querySelector("body");
 
   if (element.classList.contains("active")) {
     element.classList.remove("active");
+    mainBody.style.overflowX = "unset";
   } else {
     element.classList.add("active");
+    mainBody.style.overflowX = "hidden";
+  }
+}
+
+function onMenuClick() {
+  var element = document.getElementById("top-header");
+
+  if (element.classList.contains("active")) {
+    element.classList.remove("active");
+    mainBody.style.overflowX = "unset";
   }
 }
